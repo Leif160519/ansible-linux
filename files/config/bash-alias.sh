0bba78d5ff5da769a754a4864931e2182d49f7e2
Leif160519 <yzluofei@126.com>
2021-06-15 16:30:21 +0800

    big change

---
+++ files/config/bash-alias.sh
@@ -0,0 +1,50 @@
+alias cp="/bin/cp -a"
+alias rm="/bin/rm -I"
+alias ll="/bin/ls --color=always -hFxtrl"
+alias la="/bin/ls --color=always -hFXA"
+##
+# Is there any reason why you cannot use aliases whilst using sudo?
+# https://askubuntu.com/questions/22037/aliases-not-available-when-using-sudo
+##
+alias sudo="sudo "
+####
+
+tcp() {
+    tar cpf - ${(@)argv[1, -2]} | tar xvf - -C ${argv[-1]}
+}
+
+P() { # ps
+    ps aux | grep -v "grep" | grep -B2 "$1"
+}
+
+N() { # netstat
+    netstat -natlp | grep -v "grep" | grep "$1"
+}
+
+T() { # tail
+    tail -F "$*"
+}
+
+# archive {{{
+tgz() {
+    name=$(echo $1 | /bin/sed 's/\/*$//g')
+    tar -zcf "$name.tgz" $@
+}
+tgx() {
+    tar -zxvf $@
+}
+tgg() {
+    tar -tf $@
+}
+
+tjz() {
+    name=$(echo $1 | /bin/sed 's/\/*$//g')
+    tar -jcf "$name.tbz" $@
+}
+tjx() {
+    tar -jxvf $@
+}
+tjj() {
+    tar -tf $@
+}
+# }}}
